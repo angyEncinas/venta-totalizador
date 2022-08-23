deb41511e3fd890601aee9b241087774bb3e473e
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-08-22 21:46:53 -0400

    Func-02: Ingresar precio y mostrarlo

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,17 @@
 import sumar from "./sumador";
 
 const cantidad = document.querySelector("#ingresar-cantidad");
+const precio = document.querySelector("#precio-item");
 const form = document.querySelector("#totalizador-form");
-const div = document.querySelector("#resultado-div")
+const div1 = document.querySelector("#resultado1-div")
+const div2 = document.querySelector("#resultado2-div")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cantidaditem = Number.parseInt(cantidad.value);
+  const precioitem = Number.parseInt(precio.value);
 
-  div.innerHTML = "<p>" + cantidaditem + "</p>";
+  div1.innerHTML = "<p>" +"Cantidad: "+ cantidaditem + "</p>";
+  div2.innerHTML = "<p>" + "Precio: "+precioitem + "</p>";
 });
