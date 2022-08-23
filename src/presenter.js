f6ad12b343930541a5b313ab5402d4891710406d
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-08-22 21:38:47 -0400

    Func-01: Ingresar cantidad y mostrarla

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
 import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const cantidad = document.querySelector("#ingresar-cantidad");
+const form = document.querySelector("#totalizador-form");
+const div = document.querySelector("#resultado-div")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const cantidaditem = Number.parseInt(cantidad.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + cantidaditem + "</p>";
 });
