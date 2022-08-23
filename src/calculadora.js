ba995f54492f9af5b9f17cb9c9bbcd308f958819
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-08-22 22:15:27 -0400

    Func-04: Mostrar impuesto para UT

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,6 +1,13 @@
 function precio_neto(cantidad, precio) {
     return cantidad * precio;
   }
-  
-  export default precio_neto;
+function devolver_impuesto(estado){
+    if(estado == "UT"){
+        return 6.65;
+    }
+    else{
+        return 0
+    }
+}
+  export {precio_neto,devolver_impuesto};
   
--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,9 @@
-import precio_neto from "./calculadora";
+import {precio_neto, devolver_impuesto} from "./calculadora";
+
 
 const cantidad = document.querySelector("#ingresar-cantidad");
 const precio = document.querySelector("#precio-item");
+const estado = document.querySelector("#codigo-estado");
 const form = document.querySelector("#totalizador-form");
 const div1 = document.querySelector("#resultado1-div")
 const div2 = document.querySelector("#resultado2-div")
@@ -11,7 +13,8 @@ form.addEventListener("submit", (event) => {
 
   const cantidaditem = Number.parseInt(cantidad.value);
   const precioitem = Number.parseInt(precio.value);
+  const estadotipo = estado.value;
 
   div1.innerHTML = "<p>" +"Precio Neto: "+ precio_neto(cantidaditem,precioitem) + "</p>";
-  //div2.innerHTML = "<p>" + "Precio: "+precioitem + "</p>";
+  div2.innerHTML = "<p>" + "Impuesto para "+estadotipo+"("+devolver_impuesto(estadotipo)+ " $)" + "</p>";
 });
